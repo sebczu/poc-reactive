dc34f978d5bda2dbac553d63d3097add5953197a
sebastian.czubala.dev <sebastian.czubala.dev@gmail.com>
2020-12-06 18:43:02 +0100

    subscribe

--- application/src/test/java/com/sebczu/poc/reactive/SubscribeTest.java
+++ application/src/test/java/com/sebczu/poc/reactive/SubscribeTest.java
@@ -3,16 +3,15 @@ package com.sebczu.poc.reactive;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 @Slf4j
 @SpringBootTest
 public class SubscribeTest {
 
-
   @Test
   public void subscribe() {
-    Mono<String> publisher = Mono.just("test");
+    Flux<String> publisher = Flux.just("test", "test2");
 
     publisher
         .map(string -> {
@@ -21,6 +20,31 @@ public class SubscribeTest {
           return string;
         })
         .subscribe();
+  }
+
+  @Test
+  public void subscribe2() {
+    Flux<String> publisher = Flux.just("test", "test2");
+
+    publisher
+        .subscribe(string -> {
+          log.info("thread: " + Thread.currentThread().getName());
+          log.info(string);
+        });
+  }
+
+  @Test
+  public void subscribe3() {
+    Flux<String> publisher = Flux.just("test", "test2");
+
+    publisher
+        .subscribe(string -> {
+          log.info("thread: " + Thread.currentThread().getName());
+          log.info(string);
+        }, error -> {
+          log.info("thread: " + Thread.currentThread().getName());
+          log.error("error: ", error);
+        });
   }
 
 }
