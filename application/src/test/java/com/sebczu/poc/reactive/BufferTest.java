b411a97bcb810acdc31d73902e586d3aee7865f8
sebastian.czubala.dev <sebastian.czubala.dev@gmail.com>
2020-12-13 17:38:07 +0100

    buffer

--- application/src/test/java/com/sebczu/poc/reactive/BufferTest.java
+++ application/src/test/java/com/sebczu/poc/reactive/BufferTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+
 @Slf4j
 @SpringBootTest
 public class BufferTest {
@@ -20,6 +22,37 @@ public class BufferTest {
 
     publisher.buffer()
         .subscribe(buffered -> log.info("buffered: {}", buffered));
+  }
+
+  /**
+   * --a--b----c--d--------e--
+   * buffer
+   * ----[a,b]----[c,d]---[e]--
+   */
+  @Test
+  public void buffer2() {
+    Flux<String> publisher = Flux.just("a", "b", "c", "d", "e");
+
+    publisher.buffer(2)
+            .subscribe(buffered -> log.info("buffered: {}", buffered));
+  }
+
+  @Test
+  public void buffer3() {
+    Flux<String> publisher = Flux.just("a", "b", "c", "d", "e").delayElements(Duration.ofSeconds(1));
+
+    publisher.buffer(Duration.ofMillis(2500))
+            .doOnNext(buffered -> log.info("buffered: {}", buffered))
+            .blockLast();
+  }
+
+  @Test
+  public void buffer4() {
+    Flux<String> publisher = Flux.just("a", "b", "c", "d", "e").delayElements(Duration.ofSeconds(1));
+
+    publisher.buffer(Duration.ofMillis(700), Duration.ofMillis(1500))
+            .doOnNext(buffered -> log.info("buffered: {}", buffered))
+            .blockLast();
   }
 
 }
