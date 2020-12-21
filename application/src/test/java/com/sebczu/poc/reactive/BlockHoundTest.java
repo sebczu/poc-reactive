19b4c3b5a07f0c58630661da3f3f3561448c384f
sebastian.czubala.dev <sebastian.czubala.dev@gmail.com>
2020-12-21 21:18:26 +0100

    disable BlockHoundTest

--- application/src/test/java/com/sebczu/poc/reactive/BlockHoundTest.java
+++ application/src/test/java/com/sebczu/poc/reactive/BlockHoundTest.java
@@ -11,24 +11,24 @@ import reactor.test.StepVerifier;
 @SpringBootTest
 public class BlockHoundTest {
 
-    @Test
-    public void aaa() {
-//BlockHound.install();
-        Mono<Integer> publisher = Mono.just(1)
-                .doOnNext(value -> {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                });
-
-        StepVerifier.create(publisher)
-                .expectErrorMatches(error -> {
-                    error.printStackTrace();
-                    return error.getMessage().contains("Blocking call");
-                })
-                .verify();
-    }
+//    @Test
+//    public void aaa() {
+//        BlockHound.install();
+//        Mono<Integer> publisher = Mono.just(1)
+//                .doOnNext(value -> {
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                });
+//
+//        StepVerifier.create(publisher)
+//                .expectErrorMatches(error -> {
+//                    error.printStackTrace();
+//                    return error.getMessage().contains("Blocking call");
+//                })
+//                .verify();
+//    }
 
 }
